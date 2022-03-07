ab3b201f47cec32e75516815159f7fff4818870b
郝憨憨 <67266280+RealityError@users.noreply.github.com>
2022-03-07 19:04:04 +0800

    1

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 # 作者：realityerror
 # 创建：2022-03-06
 # 更新：2022-03-06
-# 用意：搭建网站的主题目录
+# 用意：搭建网站
 
 
 
@@ -11,6 +11,7 @@
 #网站初始化
 from asyncio.windows_events import NULL
 import os
+import pickletools
 from flask import Flask, render_template, Response, make_response, request, session
 from config import Config
 import json
@@ -24,12 +25,17 @@ from init import *
 
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD']=True
+app.config['SECRET_KEY'] = '$%^&*()345671231adFGHJBHJK,./'
+
 
 #------------------------------主页面业务-------------------------------
+#主页获取
 @app.route("/", methods=["GET", "POST"])
 def main():
     if request.method == 'GET':
         return render_template('main.html')
+
 #主页面业务-注册
 @app.route("/reg", methods=["GET", "POST"])
 def reg():
@@ -50,11 +56,11 @@ def reg():
             use_tmp.set_one(use_tmp.max_x(),2,name)
             use_tmp.set_one(use_tmp.max_x(),3,email)
             use_tmp.set_one(use_tmp.max_x(),4,password)
-            return render_template('index.html')
+            return render_template('index.html','注册成功,请登录')
         else:
             return render_template('main.html',msg="有错误")
 
-            
+
 #主页面业务-登录
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -65,20 +71,36 @@ def login():
         password = request.form.get('password_login') 
         use_tmp = database(database_user,'user')
         email_data = use_tmp.find_C(3)
+
+
         for i in range(use_tmp.user.max_column):
-            print(email_data[i])
             if email == email_data[i]:
-                print(use_tmp.find_one(i+1,4))
                 if use_tmp.find_one(i+1,4) == password:
-                    return render_template('index.html')
+                    session['username'] = use_tmp.find_one(i+1,2)
+                    picture_src = use_tmp.find_one(i+1,5)    
+                    if picture_src == None:
+                        picture_src = '/static/picture/index/ro.png'
+                    return render_template('index.html',
+                    picture = picture_src,
+                    name = use_tmp.find_one(i+1,2) 
+                    
+                    
+                    )
                 else:
                     return render_template('main.html',msg="密码错误")
         return render_template('main.html',msg="该用户不存在")    
         
-
 #----------------------------------------------------------------------
 
+#------------------------------服务端业务-------------------------------
+@app.route("/index", methods=["GET", "POST"])
+def index():
+    return render_template('index.html')
 
+@app.route("/welcome",methods=["GET", "POST"])
+def welcome():
+    return render_template('welcome.html')
+#-----------------------------------------------------------------------
 
 #主程序运行
 if __name__ == '__main__':
--- init.py
+++ init.py
@@ -1,6 +1,5 @@
 #初始化一些数据库位置
 database_user = "./database/database_user.xlsx"
+database_ro = "./database/database_ro.xlsx"
 
-#初始化行的名称
-row_table = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
